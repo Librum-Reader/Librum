81353bda21b11541d5de8e6aec1115a9daf1daf5
David <prtnprvtmail@protonmail.com>
2022-02-02 15:45:39 +0100

    Setup

---
+++ src/app/main.cpp
@@ -0,0 +1,31 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "Librum_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            app.installTranslator(&translator);
+            break;
+        }
+    }
+    
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/Librum/main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+    
+    return app.exec();
+}
---
+++ src/view/main.qml
@@ -0,0 +1,8 @@
+import QtQuick 
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Hello World")
+}
