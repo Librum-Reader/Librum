11ae6f2859c88a4ea46a0e8253aa0bdbf9ac3fe8
Creapermann <prtnprvtmail@protonmail.com>
2022-06-16 11:38:25 +0200

    Added constexpr appInformation getters

--- src/application/include/app_information.hpp
+++ src/application/include/app_information.hpp
@@ -8,20 +8,27 @@ namespace application
 class AppInformation : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QString currentVersion READ getCurrentVersion CONSTANT)
-    Q_PROPERTY(QString newestVersion READ getNewestVersion CONSTANT)
-    Q_PROPERTY(QString currentQtVersion READ getCurrentQtVersion CONSTANT)
-    Q_PROPERTY(QString companyName READ getCompanyName CONSTANT)
-    Q_PROPERTY(QString companyWebsite READ getCompanyWebsite CONSTANT)
-    Q_PROPERTY(QString companyEmail READ getCompanyEmail CONSTANT)
+    Q_PROPERTY(QString currentVersion READ getCurrentVersionAsQString CONSTANT)
+    Q_PROPERTY(QString newestVersion READ getNewestVersionAsQString CONSTANT)
+    Q_PROPERTY(QString currentQtVersion READ getCurrentQtVersionAsQString CONSTANT)
+    Q_PROPERTY(QString companyName READ getCompanyNameAsQString CONSTANT)
+    Q_PROPERTY(QString companyWebsite READ getCompanyWebsiteAsQString CONSTANT)
+    Q_PROPERTY(QString companyEmail READ getCompanyEmailAsQString CONSTANT)
     
 public:
-    static QString getCurrentVersion() { return QString::fromStdString(m_currentVersion.data()); }
-    static QString getNewestVersion() { return QString::fromStdString(m_newestVersion.data()); }
-    static QString getCurrentQtVersion() { return QString::fromStdString(m_currentQtVersion.data()); }
-    static QString getCompanyName() { return QString::fromStdString(m_companyName.data()); }
-    static QString getCompanyWebsite() { return QString::fromStdString(m_companyWebsite.data()); }
-    static QString getCompanyEmail() { return QString::fromStdString(m_companyEmail.data()); }
+    constexpr static std::string_view getCurrentVersion() { return m_currentVersion; }
+    constexpr static std::string_view getNewestVersion() { return m_newestVersion; }
+    constexpr static std::string_view getCurrentQtVersion() { return m_currentQtVersion; }
+    constexpr static std::string_view getCompanyName() { return m_companyName; }
+    constexpr static std::string_view getCompanyWebsite() { return m_companyWebsite; }
+    constexpr static std::string_view getCompanyEmail() { return m_companyEmail; }
+    
+    static QString getCurrentVersionAsQString() { return QString::fromStdString(m_currentVersion.data()); }
+    static QString getNewestVersionAsQString() { return QString::fromStdString(m_newestVersion.data()); }
+    static QString getCurrentQtVersionAsQString() { return QString::fromStdString(m_currentQtVersion.data()); }
+    static QString getCompanyNameAsQString() { return QString::fromStdString(m_companyName.data()); }
+    static QString getCompanyWebsiteAsQString() { return QString::fromStdString(m_companyWebsite.data()); }
+    static QString getCompanyEmailAsQString() { return QString::fromStdString(m_companyEmail.data()); }
     
     
 private:
