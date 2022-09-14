54f1eebee27097a98e1078b40ba319ee3c8bbdfa
Creapermann <prtnprvtmail@protonmail.com>
2022-09-14 17:33:30 +0200

    Added tag collection

--- src/domain/models/book.cpp
+++ src/domain/models/book.cpp
@@ -9,16 +9,61 @@ Book::Book(QString title)
 {
 }
 
-QString Book::title()
+
+const QString& Book::title()
 {
     return m_title;
 }
 
-void Book::setTitle(QString newTitle)
+void Book::setTitle(const QString& newTitle)
 {
     m_title = newTitle;
 }
 
+const QString& Book::localPath() const
+{
+    return m_localPath;
+}
+
+void Book::setLocalPath(const QString& newLocalPath)
+{
+    m_localPath = newLocalPath;
+}
+
+const QByteArray& Book::cover() const
+{
+    return m_cover;
+}
+
+void Book::setCover(const QByteArray& newCover)
+{
+    m_cover = newCover;
+}
+
+const QList<Tag>& Book::tags()
+{
+    return m_tags;
+}
+
+bool Book::addTag(const Tag& tag)
+{
+    auto containsTag = m_tags.contains(tag);
+    if(containsTag)
+        return false;
+    
+    m_tags.push_back(tag);
+    return true;
+}
+
+bool Book::removeTag(const Tag& tag)
+{
+    auto index = m_tags.indexOf(tag);
+    if(index == -1)
+        return false;
+    
+    m_tags.removeAt(index);
+    return true;
+}
     
 
 } // namespace domain::models
--- src/domain/models/book.hpp
+++ src/domain/models/book.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <QObject>
 #include <QString>
+#include <QByteArray>
+#include <QList>
+#include "tag.hpp"
 
 
 namespace domain::models
@@ -11,12 +14,25 @@ class Book
 public:
     Book(QString title);
     
-    QString title();
-    void setTitle(QString newTitle);
+    const QString& title();
+    void setTitle(const QString& newTitle);
+    
+    const QString& localPath() const;
+    void setLocalPath(const QString& newLocalPath);
+    
+    const QByteArray& cover() const;
+    void setCover(const QByteArray& newCover);
+    
+    const QList<Tag>& tags();
+    bool addTag(const Tag& tag);
+    bool removeTag(const Tag& tag);
     
     
 private:
     QString m_title;
+    QString m_localPath;
+    QByteArray m_cover;
+    QList<Tag> m_tags;
 };
 
 } // namespace domain::models
