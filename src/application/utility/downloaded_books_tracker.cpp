290606670cfe9fbdac1dfce0fe7b09f0507c80c9
Creapermann <prtnprvtmail@protonmail.com>
2022-10-18 12:05:55 +0200

    Added generating of user library folders

--- src/application/utility/downloaded_books_tracker.cpp
+++ src/application/utility/downloaded_books_tracker.cpp
@@ -1,5 +1,8 @@
 #include "downloaded_books_tracker.hpp"
-#include <QDir>
+#include <iterator>
+#include <ranges>
+#include <QDebug>
+#include <QFile>
 
 
 using namespace domain::models;
@@ -8,24 +11,62 @@ using namespace domain::models;
 namespace application::utility
 {
 
-std::vector<Book> DownloadedBooksTracker::getTrackedBooks(const QString& folder)
+void DownloadedBooksTracker::setLibraryOwner(const QString& libraryOwnerEmail)
+{
+    m_libraryOwnerEmail = libraryOwnerEmail;
+    m_libraryFolder = getUserLibraryDir();
+}
+
+std::vector<Book> DownloadedBooksTracker::getTrackedBooks()
+{
+    ensureUserLibraryExists();
+    
+    return {};
+}
+
+bool DownloadedBooksTracker::trackBook(const Book& book)
 {
     
 }
 
-bool DownloadedBooksTracker::trackBook(const QString& folder, const Book& book)
+bool DownloadedBooksTracker::untrackBook(const QUuid& uuid)
 {
     
 }
 
-bool DownloadedBooksTracker::untrackBook(const QString& folder, const QUuid& uuid)
+bool DownloadedBooksTracker::updateTrackedBook(const QUuid& uuid, 
+                                               const Book& book)
 {
     
 }
 
-bool DownloadedBooksTracker::updateTrackedBook(const QUuid& uuid, const Book& book)
+
+void DownloadedBooksTracker::ensureUserLibraryExists()
 {
+    auto libraryDir = getUserLibraryDir();
     
+    libraryDir.mkpath(libraryDir.path());
+}
+
+QDir DownloadedBooksTracker::getUserLibraryDir() const
+{
+    auto applicationPath = QDir::current().path();
+    auto userLibName = getUserLibraryName(m_libraryOwnerEmail);
+    
+    auto folderName = applicationPath + "/" + "librum_localLibraries" + "/"  + userLibName;
+    return QDir(folderName);
+}
+
+QString DownloadedBooksTracker::getUserLibraryName(QString email) const
+{
+    auto it = std::remove_if(email.begin(), email.end(), [](const QChar& c) { 
+        return !c.isLetterOrNumber();
+    });
+    
+    int specialCharCount = std::distance(it, email.end());
+    email.chop(specialCharCount);
+    
+    return email + QString(email.length(), '_');
 }
 
 } // namespace application::utility
--- src/application/utility/downloaded_books_tracker.hpp
+++ src/application/utility/downloaded_books_tracker.hpp
@@ -1,17 +1,36 @@
 #pragma once
+#include <QDir>
 #include "i_downloaded_books_tracker.hpp"
 
 
 namespace application::utility
 {
 
+/**
+ *  DownloadedBooksTracker maintains a set of files, where each file
+ *  contains the data of one currently downloaded book. These files are
+ *  used to keep track of books locally, so that "the local library"
+ *  (The currently downloaded books) can also be used without an active
+ *  internet connection. These local and tracked books, could then be synced
+ *  with the server, when an internet connection is established.
+ */
 class DownloadedBooksTracker : public IDownloadedBooksTracker
 {
 public:
-    std::vector<domain::models::Book> getTrackedBooks(const QString& folder) override;
-    bool trackBook(const QString& folder, const domain::models::Book& book) override;
-    bool untrackBook(const QString& folder, const QUuid& uuid) override;
-    bool updateTrackedBook(const QUuid& uuid, const domain::models::Book& book) override;
+    void setLibraryOwner(const QString& libraryOwnerEmail) override;
+    std::vector<domain::models::Book> getTrackedBooks() override;
+    bool trackBook(const domain::models::Book& book) override;
+    bool untrackBook(const QUuid& uuid) override;
+    bool updateTrackedBook(const QUuid& uuid, 
+                           const domain::models::Book& book) override;
+    
+private:
+    void ensureUserLibraryExists();
+    QDir getUserLibraryDir() const;
+    QString getUserLibraryName(QString email) const;
+    
+    QString m_libraryOwnerEmail;
+    QDir m_libraryFolder;
 };
 
 } // namespace application::utility
