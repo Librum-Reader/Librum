bda16b8b37faf1db8348382d14141fc5806aef73
Creapermann <prtnprvtmail@protonmail.com>
2022-10-19 12:51:38 +0200

    Added test for serializeToJson method

--- tests/domain_unit_tests/book_tests.hpp
+++ tests/domain_unit_tests/book_tests.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <gtest/gtest.h>
 #include <QString>
+#include <QJsonObject>
+#include <QJsonDocument>
 #include "book.hpp"
 #include "book_meta_data.hpp"
 #include "tag.hpp"
@@ -122,6 +124,58 @@ TEST(ABook, SucceedsUpdatingBook)
     EXPECT_EQ(expectedResult.getFilePath(), book.getFilePath());
     EXPECT_EQ(expectedResult.getCover(), book.getCover());
     EXPECT_EQ(expectedResult.getTags()[0], book.getTags()[0]);
+}
+
+
+
+TEST(ABook, SucceedsSerializingToJson)
+{
+    // Arrange
+    BookMetaData metaData
+    {
+        .title = "SomeTitle",
+        .author = "SomeAuthor",
+        .creator = "SomeCreator",
+        .releaseDate = "Saturday, 11. September 2021 09:17:44 UTC",
+        .format = "pdf",
+        .language = "English",
+        .documentSize = "203 KiB",
+        .pagesSize = "400 x 800",
+        .pageCount = 574,
+        .addedToLibrary = "18.10.2022 - 8:54 pm",
+        .lastOpened = "Never",
+        .cover = QImage("")
+    };
+    
+    auto uuid = QUuid::createUuid().toString(QUuid::WithoutBraces);
+    int currentPage = 224;
+    Book book("some/path", metaData, currentPage, uuid);
+    
+    // Act
+    auto serializedBook = book.serializeToJson();
+    auto jsonDoc = QJsonDocument::fromJson(serializedBook);
+    auto bookObject = jsonDoc.object();
+    
+    // Assert
+    EXPECT_EQ(metaData.title, bookObject["title"].toString());
+    EXPECT_EQ(metaData.author, bookObject["author"].toString());
+    EXPECT_EQ(metaData.creator, bookObject["creator"].toString());
+    EXPECT_EQ(metaData.releaseDate, bookObject["releaseDate"].toString());
+    EXPECT_EQ(metaData.format, bookObject["format"].toString());
+    EXPECT_EQ(metaData.language, bookObject["language"].toString());
+    EXPECT_EQ(metaData.documentSize, bookObject["documentSize"].toString());
+    EXPECT_EQ(metaData.pagesSize, bookObject["pagesSize"].toString());
+    EXPECT_EQ(metaData.pageCount, bookObject["pageCount"].toInt());
+    EXPECT_EQ(metaData.addedToLibrary, bookObject["addedToLibrary"].toString());
+    EXPECT_EQ(metaData.lastOpened, bookObject["lastOpened"].toString());
+    
+    auto cover = bookObject["cover"].toString();
+    auto coverImage = QImage::fromData(QByteArray::fromBase64(cover.toUtf8()));
+    EXPECT_EQ(metaData.cover, coverImage);
+    
+    EXPECT_EQ(book.getFilePath(), bookObject["filePath"].toString());
+    EXPECT_EQ(book.getCurrentPage(), bookObject["currentPage"].toInt());
+    EXPECT_EQ(book.getUuid(), bookObject["uuid"].toString());
 }
 
 } // namespace tests::domain
