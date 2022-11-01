0e5d5b7effd99390e84b68b7bcc09e836b8d1942
Creapermann <prtnprvtmail@protonmail.com>
2022-11-01 17:50:32 +0100

    renamed 'getCover' to 'loadCover'

--- src/application/interfaces/utility/i_book_metadata_helper.hpp
+++ src/application/interfaces/utility/i_book_metadata_helper.hpp
@@ -18,7 +18,7 @@ public:
     virtual std::optional<domain::models::BookMetaData> getBookMetaData(
         const QString& filePath) = 0;
 
-    virtual void getCover() const = 0;
+    virtual void loadCover() const = 0;
 
 signals:
     void bookCoverGenerated(const QPixmap* pixmal);
