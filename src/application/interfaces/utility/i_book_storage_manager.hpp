3917c8a6091309b6d99091ce010c14d9629662ee
Creapermann <prtnprvtmail@protonmail.com>
2022-11-20 18:29:55 +0100

    Implemented functionality for BookStorageManager

--- src/application/interfaces/utility/i_book_storage_manager.hpp
+++ src/application/interfaces/utility/i_book_storage_manager.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <QObject>
+#include <QString>
 #include <QUuid>
 #include <vector>
 #include "book.hpp"
@@ -20,8 +21,19 @@ public:
     virtual void addBook(const domain::models::Book& bookToAdd) = 0;
     virtual void deleteBook(const QUuid& uuid) = 0;
     virtual void uninstallBook(const QUuid& uuid) = 0;
-    virtual void updateBook(const domain::models::Book& newBook) = 0;
-    virtual std::vector<domain::models::Book> loadBooks() = 0;
+    virtual void updateBook(const domain::models::Book& book) = 0;
+    virtual void updateBookLocally(const domain::models::Book& book) = 0;
+    virtual void updateBookRemotely(const domain::models::Book& book) = 0;
+    virtual std::vector<domain::models::Book> loadLocalBooks() = 0;
+    virtual void loadRemoteBooks() = 0;
+
+    virtual void setUserData(const QString& email,
+                             const QString& authToken) = 0;
+    virtual void clearUserData() = 0;
+
+signals:
+    void loadingRemoteBooksFinished(
+        const std::vector<domain::models::Book>& books);
 };
 
 }  // namespace application
--- src/application/utility/book_storage_manager.hpp
+++ src/application/utility/book_storage_manager.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <QString>
 #include "i_book_storage_gateway.hpp"
 #include "i_book_storage_manager.hpp"
 #include "i_downloaded_books_tracker.hpp"
@@ -15,12 +16,18 @@ public:
     void addBook(const domain::models::Book& bookToAdd) override;
     void deleteBook(const QUuid& uuid) override;
     void uninstallBook(const QUuid& uuid) override;
-    void updateBook(const domain::models::Book& newBook) override;
-    std::vector<domain::models::Book> loadBooks() override;
+    void updateBook(const domain::models::Book& book) override;
+    void updateBookLocally(const domain::models::Book& book) override;
+    void updateBookRemotely(const domain::models::Book& book) override;
+    std::vector<domain::models::Book> loadLocalBooks() override;
+    void loadRemoteBooks() override;
+    void setUserData(const QString& email, const QString& authToken) override;
+    void clearUserData() override;
 
 private:
     IBookStorageGateway* m_bookStorageGateway;
     IDownloadedBooksTracker* m_downloadedBooksTracker;
+    QString m_authenticationToken;
 };
 
 }  // namespace application::utility
