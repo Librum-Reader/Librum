ad9b9a5d29f8c0024586455e4f7ba3484d06245a
Creapermann <prtnprvtmail@protonmail.com>
2022-12-18 16:46:43 +0100

    Changed vertical spacing

--- src/domain/models/login_model.hpp
+++ src/domain/models/login_model.hpp
@@ -17,11 +17,9 @@ private:
     bool emailIsValid() const;
     bool passwordIsValid() const;
 
-private:
     QString m_email;
     int m_minEmailLength = 6;
     int m_maxEmailLength = 50;
-
     QString m_password;
     int m_minPasswordLength = 6;
     int m_maxPasswordLength = 60;
