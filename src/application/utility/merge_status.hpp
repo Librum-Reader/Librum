6884abebe775b16190adf91183d7854736639b33
Creapermann <prtnprvtmail@protonmail.com>
2022-12-19 14:31:28 +0100

    Fixed typo in comment

--- src/application/utility/merge_status.hpp
+++ src/application/utility/merge_status.hpp
@@ -3,8 +3,8 @@
 namespace application::utility
 {
 
-// A status struct used to hold the information if the database or the local
-// library needs to be updated after a merge of two books.
+// A status struct used to hold information about whether the database or the
+// local library needs to be updated after merging two books
 struct MergeStatus
 {
     bool updateLocalLibrary = false;
