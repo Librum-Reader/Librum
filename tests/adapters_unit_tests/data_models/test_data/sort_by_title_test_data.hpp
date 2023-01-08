2d92bee18c97b8e786206daeb5027a61ef5e4a57
Creapermann <prtnprvtmail@protonmail.com>
2023-01-08 11:32:14 +0100

    Extracted 'SortByTitle' test data into a data file

--- tests/adapters_unit_tests/data_models/test_data/sort_by_title_test_data.hpp
+++ tests/adapters_unit_tests/data_models/test_data/sort_by_title_test_data.hpp
@@ -1,32 +1,23 @@
 #pragma once
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-#include <QModelIndex>
-#include <QString>
-#include "library_model.hpp"
+#include "book.hpp"
+#include "book_meta_data.hpp"
 #include "library_proxy_model.hpp"
-#include "test_data/sort_by_title_test_data.hpp"
 
-
-using namespace testing;
-using ::testing::ReturnRef;
-using namespace adapters;
-using namespace adapters::data_models;
-using namespace domain::value_objects;
-using namespace domain::entities;
-
-namespace tests::adapters
+namespace test_data
 {
 
-class ALibraryProxyModelTitleSorter
-    : public ::testing::TestWithParam<test_data::SortByTitleTestData>
+using namespace domain::entities;
+using namespace domain::value_objects;
+using namespace adapters::data_models;
+
+class SortByTitleTestData
 {
 public:
-    void SetUp() override
-    {
-    }
-
-    LibraryProxyModel libraryProxyModel;
+    Book first;
+    Book second;
+    bool expectedResult;
 };
 
 SortByTitleTestData titleSortingTestData[] = {
@@ -67,28 +58,4 @@ SortByTitleTestData titleSortingTestData[] = {
       Book("some/path", BookMetaData { .title = "Changing History" }), false },
 };
 
-TEST_P(ALibraryProxyModel, Works)
-{
-    // Arrange
-    SortByTitleTestData data = GetParam();
-    std::vector<Book> bookVec { data.first, data.second };
-
-    LibraryModel model(bookVec);
-    libraryProxyModel.setSourceModel(&model);
-    libraryProxyModel.setSortRole(LibraryProxyModel::SortRole::Title);
-
-
-    // Act
-    QModelIndex parent;
-    auto result = libraryProxyModel.lessThan(model.index(0, 0, parent),
-                                             model.index(1, 0, parent));
-
-    // Assert
-    EXPECT_EQ(data.expectedResult, result);
-}
-
-// Register the test cases
-INSTANTIATE_TEST_SUITE_P(TestSuite, ALibraryProxyModelTitleSorter,
-                         ::testing::ValuesIn(test_data::titleSortingTestData));
-
-}  // namespace tests::adapters
+}  // namespace test_data
