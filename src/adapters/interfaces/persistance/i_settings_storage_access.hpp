ecd5b6398dd7301d404737fd4030a6b3807edf65
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-01-15 19:08:23 +0100

    Renamed signal

--- src/adapters/interfaces/persistance/i_settings_storage_access.hpp
+++ src/adapters/interfaces/persistance/i_settings_storage_access.hpp
@@ -21,7 +21,7 @@ public:
                                 const QByteArray& data) = 0;
 
 signals:
-    void settingsReady(const QByteArray& data);
+    void finishedGettingSettings(const QByteArray& data);
 };
 
 }  // namespace adapters
