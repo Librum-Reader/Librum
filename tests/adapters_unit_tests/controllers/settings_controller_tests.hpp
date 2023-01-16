df806c7dd55e42d02d2f281610eff70f217d85f4
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-01-16 17:38:44 +0100

    Added 'SettingsControllerTests'

---
+++ tests/adapters_unit_tests/controllers/settings_controller_tests.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <QString>
+#include "settings_controller.hpp"
+#include "settings_service.hpp"
+
+using namespace testing;
+using namespace application;
+using namespace adapters::controllers;
+
+namespace tests::adapters
+{
+
+class SettingsServiceMock : public application::ISettingsService
+{
+public:
+    MOCK_METHOD(QString, getSetting, (const QString&), (override));
+    MOCK_METHOD(void, setSetting, (const QString&, const QString&), (override));
+    MOCK_METHOD(void, loadUserSettings, (const QString&, const QString&),
+                (override));
+    MOCK_METHOD(void, clearUserData, (), (override));
+};
+
+struct ASettingsController : public ::testing::Test
+{
+    ASettingsController()
+    {
+    }
+
+    void SetUp() override
+    {
+        settingsController =
+            std::make_unique<SettingsController>(&settingsServiceMock);
+    }
+
+    SettingsServiceMock settingsServiceMock;
+    std::unique_ptr<SettingsController> settingsController;
+};
+
+TEST_F(ASettingsController, SucceedsGettingSetting)
+{
+    // Arrange
+    QString settingName = "SomeSetting";
+
+    // Expect
+    EXPECT_CALL(settingsServiceMock, getSetting(_)).Times(1);
+
+    // Act
+    settingsController->getSetting(settingName);
+}
+
+TEST_F(ASettingsController, SucceedsSettingSetting)
+{
+    // Arrange
+    QString settingName = "SomeSetting";
+    QString settingValue = "SomeSetting";
+
+    // Expect
+    EXPECT_CALL(settingsServiceMock, setSetting(_, _)).Times(1);
+
+    // Act
+    settingsController->setSetting(settingName, settingValue);
+}
+
+}  // namespace tests::adapters
