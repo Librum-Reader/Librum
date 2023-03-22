8902e68bc30d3ec746e856995ae6e33b820e2dc7
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-03-22 23:12:07 +0100

    Removed qquicktableviewextra

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,6 @@
 #include "library_proxy_model.hpp"
 #include "message_handler.hpp"
 #include "page_item.hpp"
-#include "qquicktableviewextra_p.h"
 #include "setting_groups.hpp"
 #include "setting_keys.hpp"
 #include "sidebar_state.hpp"
@@ -66,7 +65,6 @@ int main(int argc, char* argv[])
     qmlRegisterType<cpp_elements::KeySequenceRecorder>("Librum.elements", 1, 0, "KeySequenceRecorder");
     qmlRegisterType<cpp_elements::PageItem>("Librum.elements", 1, 0, "PageItem");
     qmlRegisterType<cpp_elements::DocumentItem>("Librum.elements", 1, 0, "DocumentItem");
-    qmlRegisterType<QQuickTableViewExtra>("Librum.elements", 1, 0, "TableViewExtra");
     qRegisterMetaType<adapters::dtos::BookDto>();
     qRegisterMetaType<adapters::dtos::TagDto>();
     qRegisterMetaType<adapters::dtos::UserDto>();
