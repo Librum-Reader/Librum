514ddcfb465e813c346dbff3327eca99d08cecbc
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-04-05 17:01:43 +0200

    Fixed url's

--- src/infrastructure/data/endpoints.hpp
+++ src/infrastructure/data/endpoints.hpp
@@ -4,29 +4,27 @@
 namespace infrastructure::data
 {
 
-inline const QString baseUrl { "https://localhost:7084" };
+inline const QString domain { "https://localhost:7084" };
 
 // Authentication
-inline const QString authenticationEndpoint { baseUrl + "/api/login" };
-inline const QString registrationEndpoint { baseUrl + "/api/register" };
+inline const QString authenticationEndpoint { domain + "/api/login" };
+inline const QString registrationEndpoint { domain + "/api/register" };
 
 // Book storage
-inline const QString bookCreationEndpoint { baseUrl + "/api/book" };
-inline const QString uploadBookDataEndpoint { baseUrl +
-                                              "/api/book/addBookBinaryData" };
-inline const QString downloadBookDataEndpoint { baseUrl +
-                                                "/api/book/getBookBinaryData" };
-inline const QString bookUpdateEndpoint { baseUrl + "/api/book" };
-inline const QString bookDeletionEndpoint { baseUrl + "/api/book" };
-inline const QString booksMetadataGetEndpoint { baseUrl + "/api/book" };
+inline const QString bookCreationEndpoint { domain + "/api/book" };
+inline const QString uploadBookDataEndpoint { domain + "/api/book/bookData" };
+inline const QString downloadBookDataEndpoint { domain + "/api/book/bookData" };
+inline const QString bookUpdateEndpoint { domain + "/api/book" };
+inline const QString bookDeletionEndpoint { domain + "/api/book" };
+inline const QString booksMetadataGetEndpoint { domain + "/api/book" };
 
 // User storage
-inline const QString userGetEndpoint { baseUrl + "/api/user" };
-inline const QString userPatchEndpoint { baseUrl + "/api/user" };
+inline const QString userGetEndpoint { domain + "/api/user" };
+inline const QString userPatchEndpoint { domain + "/api/user" };
 
 // Tag storage
-inline const QString tagUpdateEndpoint { baseUrl + "/api/tag" };
-inline const QString tagDeletionEndpoint { baseUrl + "/api/tag" };
+inline const QString tagUpdateEndpoint { domain + "/api/tag" };
+inline const QString tagDeletionEndpoint { domain + "/api/tag" };
 
 
 }  // namespace infrastructure::data
