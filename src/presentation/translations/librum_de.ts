4d5726add393546116872d33740154fcd6047f35
David Lazarescu <prtnprvtmail@protonmail.com>
2023-11-07 17:56:51 +0100

    Added translations

---
+++ src/presentation/translations/librum_de.ts
@@ -0,0 +1,205 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>MAcceptPolicy</name>
+    <message>
+        <location filename="../registerPage/MAcceptPolicy.qml" line="48"/>
+        <source>I accept the</source>
+        <extracomment>Make sure to translate the following words together to make a logical sentence</extracomment>
+        <translation>Ich akzeptiere die</translation>
+    </message>
+    <message>
+        <source> Terms of Service</source>
+        <translation type="vanished">AGB</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MAcceptPolicy.qml" line="50"/>
+        <source>Terms of Service</source>
+        <translation>AGB</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MAcceptPolicy.qml" line="51"/>
+        <source>and the</source>
+        <translation>und</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MAcceptPolicy.qml" line="53"/>
+        <source>Privacy Policy</source>
+        <translation>Datenschutzrichtlinien</translation>
+    </message>
+</context>
+<context>
+    <name>MForgotPasswordPage</name>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="75"/>
+        <source>Forgot Password</source>
+        <translation>Passwort Vergessen</translation>
+    </message>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="87"/>
+        <source>Enter your email and we&apos;ll send you a link to reset your password</source>
+        <translation>Gib deine E-Mail-Adresse ein und wir sicken dir einen Link um dein Passwort zurückzusetzen</translation>
+    </message>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="130"/>
+        <source>Send Email</source>
+        <translation>Email senden</translation>
+    </message>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="147"/>
+        <source>Back to Login</source>
+        <translation>Zurrück zum Login</translation>
+    </message>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="173"/>
+        <source>Email sent! Keep an eye on your inbox</source>
+        <translation>Email gesendet! Behalte dein Postfach im Auge</translation>
+    </message>
+    <message>
+        <source>Email sent! Keep an eye on your inbox.</source>
+        <translation type="vanished">Email gesendet! Behalte dein Postfach im Auge</translation>
+    </message>
+</context>
+<context>
+    <name>MLoginPage</name>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="96"/>
+        <source>Welcome back!</source>
+        <translation>Willkommen zurrück!</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="106"/>
+        <source>Log into your account</source>
+        <translation>Melde dich in deinem Konto an</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="117"/>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="132"/>
+        <source>Password</source>
+        <translation>Passwort</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="178"/>
+        <source>Remember me</source>
+        <translation>Angemeldet bleiben</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="198"/>
+        <source>Forgot password?</source>
+        <translation>Passwort vergessen?</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="225"/>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="249"/>
+        <source>Don&apos;t have an account? Register</source>
+        <translation>Noch kein Konto? Registrieren</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="268"/>
+        <source>We&apos;re Sorry</source>
+        <translation>Es tut uns leid</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="269"/>
+        <source>Logging you in failed, please try again later.</source>
+        <translation>Die Anmeldung ist fehlgeschlagen, bitte versuche es später erneut.</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="270"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="271"/>
+        <source>Report</source>
+        <translation>Melden</translation>
+    </message>
+</context>
+<context>
+    <name>MRegisterPage</name>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="79"/>
+        <source>Welcome!</source>
+        <translation>Willkommen!</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="90"/>
+        <source>Your credentials are only used to authenticate yourself. Everything will be stored in a secure database.</source>
+        <translation>Deine Daten werden nur zur Authentifizierung verwendet. Alles wird sicher gespeichert.</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="120"/>
+        <source>First name</source>
+        <translation>Vorname</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="133"/>
+        <source>Last name</source>
+        <translation>Nachname</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="148"/>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="162"/>
+        <source>Password</source>
+        <translation>Passwort</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="177"/>
+        <source>Confirmation password</source>
+        <translation>Passwort erneut eingeben</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="215"/>
+        <source>Let&apos;s start</source>
+        <translation>Lass uns loslegen</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="237"/>
+        <source>Already have an account? Login</source>
+        <translation>Du hast bereits ein Konto? Einloggen</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="263"/>
+        <source>Confirm Your Email</source>
+        <translation>Email bestätigen</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="264"/>
+        <source>You&apos;re are almost ready to go!
+Confirm your email by clicking the link we sent you.</source>
+        <translation>Du bist fast bereit loszulegen!
+Bestätige deine E-Mail, indem du auf den von uns gesendeten Link klickst.</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="265"/>
+        <source>Resend</source>
+        <translation>Nochmal</translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="296"/>
+        <source>Passwords don&apos;t match</source>
+        <translation>Passwörter stimmen nicht überein</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="31"/>
+        <source>Librum - Your ebook reader</source>
+        <translation>Librum - Dein e-Buch leser</translation>
+    </message>
+</context>
+</TS>
---
+++ src/presentation/translations/librum_it.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MAcceptPolicy</name>
+    <message>
+        <location filename="../registerPage/MAcceptPolicy.qml" line="48"/>
+        <source>I accept the</source>
+        <extracomment>Make sure to translate the following words together to make a logical sentence</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MAcceptPolicy.qml" line="50"/>
+        <source>Terms of Service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MAcceptPolicy.qml" line="51"/>
+        <source>and the</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MAcceptPolicy.qml" line="53"/>
+        <source>Privacy Policy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MForgotPasswordPage</name>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="75"/>
+        <source>Forgot Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="87"/>
+        <source>Enter your email and we&apos;ll send you a link to reset your password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="130"/>
+        <source>Send Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="147"/>
+        <source>Back to Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../forgotPasswordPage/MForgotPasswordPage.qml" line="173"/>
+        <source>Email sent! Keep an eye on your inbox</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MLoginPage</name>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="96"/>
+        <source>Welcome back!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="106"/>
+        <source>Log into your account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="117"/>
+        <source>Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="132"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="178"/>
+        <source>Remember me</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="198"/>
+        <source>Forgot password?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="225"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="249"/>
+        <source>Don&apos;t have an account? Register</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="268"/>
+        <source>We&apos;re Sorry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="269"/>
+        <source>Logging you in failed, please try again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="270"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../loginPage/MLoginPage.qml" line="271"/>
+        <source>Report</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MRegisterPage</name>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="79"/>
+        <source>Welcome!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="90"/>
+        <source>Your credentials are only used to authenticate yourself. Everything will be stored in a secure database.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="120"/>
+        <source>First name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="133"/>
+        <source>Last name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="148"/>
+        <source>Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="162"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="177"/>
+        <source>Confirmation password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="215"/>
+        <source>Let&apos;s start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="237"/>
+        <source>Already have an account? Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="263"/>
+        <source>Confirm Your Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="264"/>
+        <source>You&apos;re are almost ready to go!
+Confirm your email by clicking the link we sent you.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="265"/>
+        <source>Resend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../registerPage/MRegisterPage.qml" line="296"/>
+        <source>Passwords don&apos;t match</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="31"/>
+        <source>Librum - Your ebook reader</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
